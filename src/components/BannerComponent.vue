25ca95cb4fde75925a6aaef4ef62a36e6157226b
Jeferson-Filho <jpdietrichfilho.2015@gmail.com>
2024-10-11 01:14:00 -0300

    :sparkles: feat: banner components

---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="banner">
+        <picture class="banner-image">
+            <source :srcset="banner" media="(max-width: 1023px)" />
+            <source :srcset="banner" media="(min-width: 1024px)" />
+            <img :src="banner" alt="Banner" class="banner-image" />
+        </picture>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BannerComponent',
+        props: {
+            banner: {
+                type: String,
+                required: true, // A prop é obrigatória para garantir que o caminho da imagem seja fornecido
+            },
+        },
+    }
+</script>
+
+<style scoped>
+.banner {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    max-height: 600px;
+}
+
+.banner-image {
+    width: 100%;
+    height: auto;
+}
+</style>
